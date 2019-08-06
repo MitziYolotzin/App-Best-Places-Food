a956608623689a21f60240a475516aabe2db14aa
MitziYolotzin <ultravisuell@gmail.com>
2019-08-06 00:36:57 -0500

    indent code

--- src/app.js
+++ src/app.js
@@ -1,27 +1,23 @@
-
-
 //
-  
-const dataView  = document.getElementById('places-data');
-const searchName = document.getElementById('search-name');
-const order = document.getElementById('order');
+
+const dataView = document.getElementById("places-data");
+const searchName = document.getElementById("search-name");
+const order = document.getElementById("order");
 
 //
 
-    fetch('https://app-food-83580.firebaseio.com/appfood.json')
-    .then(response => response.json())
-    .then(dataRestaurant => {
-        console.log(dataRestaurant)
-      localStorage.dataRestaurant = JSON.stringify(dataRestaurant.appfood);
-     showList(dataRestaurant);
-      //return dataRestaurant
-    });
-  
- 
-  
-  //Print the data
-  const printPlacesFood = (appfood) => {
-    let namePlaceFood = `
+fetch("https://app-food-83580.firebaseio.com/appfood.json")
+  .then(response => response.json())
+  .then(dataRestaurant => {
+    console.log(dataRestaurant);
+    localStorage.dataRestaurant = JSON.stringify(dataRestaurant.appfood);
+    showList(dataRestaurant);
+    //return dataRestaurant
+  });
+
+//Print the data
+const printPlacesFood = appfood => {
+  let namePlaceFood = `
     <div id="restaurant-info">
     <i class="fas fa-utensils"></i><h3>Restaurant</h3> 
     <p>${appfood.name}</p>
@@ -42,32 +38,38 @@ const order = document.getElementById('order');
     <br></br>
     </div>
     `;
-    dataView.insertAdjacentHTML("beforeend", namePlaceFood);
-  };
- 
-  //Show List Data
-  const showList = (dataPlaceList) => {
-    dataView.innerHTML = "";
-    dataPlaceList.forEach(element => {
-      printPlacesFood(element);
-    });
-  };
+  dataView.insertAdjacentHTML("beforeend", namePlaceFood);
+};
 
-  //Filter coincidence 
+//Show List Data
+const showList = dataPlaceList => {
+  dataView.innerHTML = "";
+  dataPlaceList.forEach(element => {
+    printPlacesFood(element);
+  });
+};
+
+//Filter coincidence
 const filterCoincidence = () => {
-    searchName.addEventListener('keyup', () => {
-      let searchValue = document.getElementById('search-name').value;
-      showList(window.data.filterByLetter(JSON.parse(localStorage.dataRestaurant), searchValue));
-    });
-  }
-  
+  searchName.addEventListener("keyup", () => {
+    let searchValue = document.getElementById("search-name").value;
+    showList(
+      window.data.filterByLetter(
+        JSON.parse(localStorage.dataRestaurant),
+        searchValue
+      )
+    );
+  });
+};
 
-  const getOrderPlace = () => {
-   
-      order.addEventListener("click", (event) => {
-        
-        showList(window.data.sortData(JSON.parse(localStorage.getItem("dataRestaurant")), event.target.name));
-      });
-    
-  }
-  getOrderPlace()
+const getOrderPlace = () => {
+  order.addEventListener("click", event => {
+    showList(
+      window.data.sortData(
+        JSON.parse(localStorage.getItem("dataRestaurant")),
+        event.target.name
+      )
+    );
+  });
+};
+getOrderPlace();
